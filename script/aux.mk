f93f89e3d2d2feb9341747026c04dbb712d71e11
root <root@Edgar-PC.(none)>
2014-04-18 18:00:40 +0900

    Version Makefiles system ready to be tested

--- script/aux.mk
+++ script/aux.mk
@@ -36,7 +36,7 @@ GCC   ?= g++
 #CLANG ?= /usr/bin/clang
 
 ifeq ("$(OSUPPER)","LINUX")
-     NVCC ?= $(CUDA_PATH)/bin/nvcc -ccbin $(GCC)
+     NVCC ?= /usr/local/cuda/bin/nvcc -ccbin $(GCC)
 endif
 
 # Take command line flags that override any of these settings
@@ -56,15 +56,15 @@ endif
 ifeq ("$(OSUPPER)","LINUX")
     # Each Linux Distribuion has a set of different paths.  This applies especially when using the Linux RPM/debian packages
     ifeq ("$(DISTRO)","ubuntu")
-        CUDA_PATH  	:= /usr/local/cuda
-				CUDA_SDKPATH := $(CUDA_PATH)/NVIDIA_GPU_Computing_SDK
-				DSCUDA_PATH	:= /usr/local/DSCUDA/dscudapkg$(DSCUDA_VER)
+        CUDA_PATH  		:= /usr/local/cuda
+				CUDA_SDKPATH 	:= $(CUDA_PATH)/NVIDIA_GPU_Computing_SDK
+				DSCUDA_PATH		:= /usr/local/DSCUDA/dscudapkg$(DSCUDA_VER)
     endif
 		#For Knoppix..............
     ifeq ("$(DISTRO)","debian")
-        CUDA_PATH  	:= /usr/local/cuda
-				CUDA_SDKPATH := $(CUDA_PATH)/NVIDIA_GPU_Computing_SDK
-				DSCUDA_PATH	:= /usr/local/DSCUDA/dscudapkg$(DSCUDA_VER)
+        CUDA_PATH  		:= /usr/local/cuda
+				CUDA_SDKPATH 	:= $(CUDA_PATH)/NVIDIA_GPU_Computing_SDK
+				DSCUDA_PATH		:= /usr/local/DSCUDA/dscudapkg$(DSCUDA_VER)
     endif
   # Search for Linux distribution path for libcuda.so
   CUDALIB ?= $(shell find $(CUDAPATH) $(DFLT_PATH) -name libcuda.so -print 2>/dev/null)

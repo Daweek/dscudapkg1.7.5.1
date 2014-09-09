893d4a6040f9d6636a23550073565597cb72a8a3
System Administrator <root@Mac-Edgar.local>
2014-09-09 15:31:21 +0300

    Uploading from MacPro... DSCUDA tested on Mac The hint is to install gnu gcc from website

--- sample/vecadd/vecadd.cuh
+++ sample/vecadd/vecadd.cuh
@@ -4,11 +4,3 @@ vecAdd(float *a, float *b, float *c)
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     c[i] = a[i] + b[i];
 }
-
-__global__ void
-vecMul(float *a, float *b, float c, float *d, int e, int * f)
-{
-    int i = threadIdx.x;
-    d[i] = a[i] * b[i] + c + e + f[i];
-}
-
